8dddb5e5b44d4e42cf04e1ed128af712c448cbd1
iykeevans <elochi238@gmail.com>
2018-10-23 23:00:35 +0100

    imported and used form component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Form from './components/Form'
 
 class App extends Component {
+  
+
   render() {
     return (
       <div className="App">
-        HelloWorld
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+        </header>
+        <Form />
       </div>
     );
   }
